78442a2e2910c1330c6b26130dd0d41296f10136
zhangyao <ylzhangyao@gmail.com>
2018-12-15 21:14:40 +0800

    From MAC Pycharm

--- appdir/app/views.py
+++ appdir/app/views.py
@@ -4,3 +4,16 @@ from app import app
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+@app.route('/demolove/')
+def demolove():
+    return render_template('./demo/love.html')
+
+
+@app.route('/oriyao/')
+def oriyao():
+    return render_template('./demo/oriyao.html')
+
+@app.route('/item/')
+def item():
+    return render_template('./item/item.html')
